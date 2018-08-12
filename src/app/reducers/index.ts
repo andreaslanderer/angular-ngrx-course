06ad932545b101cdb139d40c60d7448a9218d152
Andreas Landerer <andreas.landerer@gmail.com>
2018-08-12 20:45:22 +0200

    Lecture 11: Added reducer to manipulate AppState in case of a login action

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,11 +1,34 @@
 import {ActionReducerMap, MetaReducer} from '@ngrx/store';
 import {environment} from '../../environments/environment';
+import {User} from '../model/user.model';
+import {AuthActionTypes} from '../auth/auth.actions';
 
-export interface AppState {
-
+export interface AuthState {
+  loggedIn: boolean;
+  user: User;
 }
 
-export const reducers: ActionReducerMap<AppState> = {};
+export interface AppState {
+  auth: AuthState;
+}
+
+export const reducers: ActionReducerMap<AppState> = {
+  auth: authReducer
+};
+
+function authReducer(state: AuthState, action): AuthState {
+  switch (action.type) {
+    case AuthActionTypes.LoginAction: {
+      return {
+        loggedIn: true,
+        user: action.payload.user
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
 
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];

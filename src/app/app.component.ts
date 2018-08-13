f68b16c96ec80276ba226f647201e4d8c6206bac
Andreas Landerer <andreas.landerer@gmail.com>
2018-08-13 12:21:44 +0200

    Introduced observables to determine whether login and logout buttons are to be rendered

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {AppState} from './reducers';
 import {Logout} from './auth/auth.actions';
+import {map} from 'rxjs/operators';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +12,23 @@ import {Logout} from './auth/auth.actions';
 })
 export class AppComponent implements OnInit {
 
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
 
   constructor(private store: Store<AppState>) {
 
   }
 
   ngOnInit() {
-
+    this.isLoggedIn$ = this.store.pipe(
+      map(state => (state as any).auth.loggedIn)
+    );
+    this.isLoggedOut$ = this.store.pipe(
+      map(state => !(state as any).auth.loggedIn)
+    );
   }
 
   logout() {
-      this.store.dispatch(new Logout());
+    this.store.dispatch(new Logout());
   }
 }

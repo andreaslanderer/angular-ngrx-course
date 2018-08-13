b1428271c2f3519f21b95ab24966964dcb51fae6
Andreas Landerer <andreas.landerer@gmail.com>
2018-08-13 10:29:23 +0200

    Lecture 13: Used ngrx schematics to generate the AuthState and the LoginReducer

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,39 +1,13 @@
 import {ActionReducerMap, MetaReducer} from '@ngrx/store';
 import {environment} from '../../environments/environment';
-import {User} from '../model/user.model';
-import {AuthActionTypes} from '../auth/auth.actions';
-
-export interface AuthState {
-  loggedIn: boolean;
-  user: User;
-}
 
 export interface AppState {
-  auth: AuthState;
+
 }
 
 export const reducers: ActionReducerMap<AppState> = {
-  auth: authReducer
-};
 
-const initialAuthState: AuthState = {
-  loggedIn: false,
-  user: undefined,
 };
-
-function authReducer(state: AuthState = initialAuthState, action): AuthState {
-  switch (action.type) {
-    case AuthActionTypes.LoginAction: {
-      return {
-        loggedIn: true,
-        user: action.payload.user
-      };
-    }
-    default: {
-      return state;
-    }
-  }
-}
 
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production ? [] : [];
